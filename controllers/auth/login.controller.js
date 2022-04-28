3cc0391da9d57a0bfce9d6edd352301fe789f908
Camilo López <camiloj.lopez79@gmail.com>
2022-04-27 22:25:40 -0400

    Se crea el controlador para login

--- controllers/auth/login.controller.js
+++ controllers/auth/login.controller.js
@@ -1,15 +1,39 @@
+const bcrypt = require("bcryptjs");
 const { response, request } = require("express");
+const User = require( '../../models/user.model' );
 
 /*
   PATH: /api/auth/login
   DOC: 
 */
-const login = ( req = request, res = response ) => {
+const login = async ( req = request, res = response ) => {
+  const { email, password } = req.body;
+
   try {
+    // Check if email exists
+    const user = await User.findOne({ email });
+    if ( !user ) {
+      return res.status( 401 ).json({
+        ok: false,
+        msg: 'Incorrect email or password'
+      });
+    }
+
+    // Check Password
+    const validPassword = bcrypt.compareSync( password, user.password );
+    if ( !validPassword ) {
+      return res.status( 401 ).json({
+        ok: false,
+        msg: 'Incorrect email or password'
+      });
+    }
+
+    //TODO: Generate JWT
 
     res.status( 200 ).json({
       ok: true,
-      msg: 'Login Success'
+      user,
+      //token
     });
 
   } catch ( err ) {
